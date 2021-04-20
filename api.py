d97bb0f0565e7ed3e55d9cf3a57b6f4a758cea60
Theo Billet <billettheo5@gmail.com>
2021-04-20 10:00:39 +0200

    update function png and add new hmtl page : error

--- api.py
+++ api.py
@@ -11,6 +11,7 @@ with open('./config.json', 'r') as f:
 	config = json.load(f)
 folder_rawls_path = config['path']
 scene_list = [ f for f in os.listdir(folder_rawls_path) if os.path.isdir(os.path.join(folder_rawls_path,f)) ]
+errors = ["ERROR : Your name of the scene doesn't exist"]
 
 @app.route("/home")
 @app.route("/")
@@ -24,11 +25,14 @@ def list():
 @app.route("/<name_scene>/png/ref")
 def png(name_scene=None):
     if name_scene not in scene_list:
-        return render_template("home.html") #à faire
+        return render_template("error.html", error = errors[0]) #à faire
     if not os.path.exists("static/images/" + name_scene + ".png"):
-        rawls_img = Rawls.load(folder_rawls_path + "/" + name_scene + "/p3d_cornel-box-view0-S20-00000.rawls")
+        for f in os.listdir(folder_rawls_path + "/" + name_scene):
+            first_file = f
+            break
+        rawls_img = Rawls.load(folder_rawls_path + "/" + name_scene + "/" + first_file)
         rawls_img.save("static/images/" + name_scene + ".png")
-    return render_template("png_image.html",name_scene = name_scene)
+    return render_template("png_image.html",name_scene = name_scene, image_png = "images/"+name_scene+".png")
 
 if __name__ == "__main__":
     app.run(debug=True)
