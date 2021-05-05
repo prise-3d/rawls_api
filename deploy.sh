4b97713e1aad171230ef5bf616e04230ffde602c
Jérôme BUISINE <contact@jeromebuisine.fr>
2021-05-05 14:19:33 +0200

    update deploy script and add json config file

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,13 @@ fi
 docker build --no-cache -t api-rawls  .
 
 # Lancer le container
-if [[ "$(docker ps -q rawls_api_instance 2> /dev/null)" != "" ]]; then
+if [[ "$(docker ps | grep rawls_api_instance 2> /dev/null)" != "" ]]; then
     docker stop rawls_api_instance
     docker rm rawls_api_instance
 fi
+
+if [[ "$(docker ps -a | grep rawls_api_instance 2> /dev/null)" != "" ]]; then
+    docker rm rawls_api_instance
+fi
+
 docker run --name rawls_api_instance -p 5001:5001 api-rawls
